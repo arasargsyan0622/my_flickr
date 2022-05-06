3dd1129d4bccf3fef2f08949ac2e7f8e1718b121
ara <arasargsyan0531@gmail.com>
2022-05-05 23:02:06 -0700

    seeders updated

--- backend/db/seeders/20220503025443-imageData.js
+++ backend/db/seeders/20220503025443-imageData.js
@@ -10,17 +10,17 @@ module.exports = {
       return queryInterface.bulkInsert('Images', [
         {
           title: 'Kepler-22b',
-          imageUrl: '1068e845d682d222bed078026bf87025',
+          imageUrl: 'https://spickr.s3.us-west-1.amazonaws.com/1651549488959.jpg',
           userId: 2,
         },
         {
           title: 'Kepler-22b',
-          imageUrl: "1068e845d682d222bed078026bf87025",
+          imageUrl: "https://spickr.s3.us-west-1.amazonaws.com/1651549594588.png",
           userId: 1,
         },
         {
           title: 'Kepler-22b',
-          imageUrl: "1068e845d682d222bed078026bf87025",
+          imageUrl: "https://spickr.s3.us-west-1.amazonaws.com/1651550138609.jpg",
           userId: 1,
         },
         {
